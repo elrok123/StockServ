6d55535820cbe73f5e301ed34878cb94fe576051
Conner McCabe <conner@koansystems.co.uk>
2014-11-27 13:18:49 +0000

    Added methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		data = YahooFinance.quotes(["BVSP", "NATU3.SA", "USDJPY=X"], [:ask, :bid, :last_trade_date])
+		@data = YahooFinance.quotes(["BVSP", "NATU3.SA", "USDJPY=X"], [:ask, :bid, :last_trade_date])
 	end
 end
