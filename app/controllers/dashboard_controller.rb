1bfe1d1c80af3ce5b678b5d591318934eea6f7e1
Conner McCabe <conner@koansystems.co.uk>
2014-11-28 11:33:37 +0000

    Added some stock related stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date])
+		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date, :close, :dividend_yield])
 	end
 end
