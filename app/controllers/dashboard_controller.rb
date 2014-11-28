9f91c6db900d9ae2247078489fbfb46a8d605626
Conner McCabe <conner@koansystems.co.uk>
2014-11-28 12:15:16 +0000

    Removed ambiguous group_by

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.historical_quotes(["GOOG"], { start_date: Time::now-(24*60*60*10), end_date: Time::now }).group_by()
+		@data = YahooFinance.historical_quotes(["GOOG"], { start_date: Time::now-(24*60*60*10), end_date: Time::now })
 	end
 end
