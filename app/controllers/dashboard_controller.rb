698918f15e452624a8e9ef5df726e7f19283a1be
Conner McCabe <conner@koansystems.co.uk>
2014-11-28 12:16:09 +0000

    Changed array to string

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.historical_quotes(["GOOG"], { start_date: Time::now-(24*60*60*10), end_date: Time::now })
+		@data = YahooFinance.historical_quotes("GOOG", { start_date: Time::now-(24*60*60*10), end_date: Time::now })
 	end
 end
