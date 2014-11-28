6f69732aae790c60f3c18d2ac6df48db16dd4818
Conner McCabe <conner@koansystems.co.uk>
2014-11-28 11:35:37 +0000

    More stock stats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date, :close, :dividend_yield])
+		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date, :close, :dividend_per_share])
 	end
 end
