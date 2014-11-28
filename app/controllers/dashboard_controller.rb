01211df486fe2b75e699043b38456fbdeb6a6c89
Conner McCabe <conner@koansystems.co.uk>
2014-11-28 11:46:24 +0000

    Added real time change

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date, :close, :dividend_per_share, :day_value_change])
+		@data = YahooFinance.quotes(["GOOG"], [:ask, :bid, :last_trade_date, :close, :dividend_per_share, :day_value_change, :change_real_time])
 	end
 end
