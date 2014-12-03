dcccbb835a67bbb559235c884dce1a4f531c2ce1
Conner Stephen McCabe <conner@koansystems.co.uk>
2014-12-03 00:15:04 +0000

    Removed locally installed gems due to heavy repo download issues. Please install gems using bundle install.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
 	def show
 		@data = YahooFinance.historical_quotes("GOOG", { start_date: 10.days.ago, end_date: Time::now })
+		@ugis = "Loves men"
 	end
 end
