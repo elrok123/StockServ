64371e3ebe9bba5e89b8ff3b74fcd78c6433cafa
Conner Stephen McCabe <conner@koansystems.co.uk>
2014-12-03 00:36:44 +0000

    Got everything upto date after users pushed to wrong branch.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
-  def show
-  end
+	def show
+		@data = YahooFinance.historical_quotes("GOOG", { start_date: 10.days.ago, end_date: Time::now })
+	end
 end
