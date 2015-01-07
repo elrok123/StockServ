ff08f86be7332655eb21e3cdc895bda281f8fce8
Conner Stephen McCabe <conner@koansystems.co.uk>
2015-01-07 04:19:03 +0000

    Added some stuff, and removed some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/login'
+
+  get 'users/logout'
+
+  get 'sessions/login'
+
+  get 'sessions/logout'
+
 	get 'dashboard/show'
 	
 	root to: 'dashboard#show'
