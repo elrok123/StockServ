00366909793b203212ef37e2084f4759649fbb9c
Jack Pryde <jwp1@hw.ac.uk>
2015-01-08 06:16:45 +0000

    Plugged the yahoo api into the graph and made graph look nicer

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.historical_quotes("GOOG", { start_date: 10.days.ago, end_date: Time::now })
+		@data = YahooFinance.historical_quotes("GOOG", { start_date: 20.days.ago, end_date: Time::now})
+		@high = Hash.new()
+		@low = Hash.new()
+		@open = Hash.new()
+		@info_array = Array.new()
+		@highest
+		@lowest
 	end
 end
