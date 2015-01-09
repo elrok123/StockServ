bd17386a7a76a875cdedab9756b1cd4bced45028
Conner Stephen McCabe <conner@koansystems.co.uk>
2015-01-09 07:33:40 +0000

    Added min/max calculations for graphing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,12 @@
 class DashboardController < ApplicationController
 	def show
-		@data = YahooFinance.historical_quotes("GOOG", { start_date: 20.days.ago, end_date: Time::now})
+		@data = YahooFinance.historical_quotes("GOOG", { start_date: 50.days.ago, end_date: Time::now})
 		@high = Hash.new()
 		@low = Hash.new()
 		@open = Hash.new()
 		@info_array = Array.new()
 
+		#Compile highs and lows tuples for graph
 		@data.each do |e| 
 			@high[e.trade_date] = e.high 
 			@low[e.trade_date] = e.low 
@@ -13,6 +14,12 @@ class DashboardController < ApplicationController
 		end 
 
 		@info_array = ["High", @high, "Low", @low, "Open", @open]
-		
+
+		#Calculate highest and lowest stock figures to set min and max on the graph
+		@i = -9999999.99 
+		@info_array[1].map {|k, v| @i = v.gsub(/[^\d^\.]/, '').to_f unless @i > v.gsub(/[^\d^\.]/, '').to_f } 
+		@highest = (@i + 50.0).round(-2) 
+		@info_array[3].map {|k, v| @i = v.gsub(/[^\d^\.]/, '').to_f unless @i < v.gsub(/[^\d^\.]/, '').to_f } 
+		@lowest = (@i - 50.0).round(-2) 
 	end
 end
