35ad6cb8c734741d6afdf19ae802dcf72a777523
Conner Stephen McCabe <conner@koansystems.co.uk>
2015-01-09 06:39:55 +0000

    Adding changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,30 @@ class DashboardController < ApplicationController
 		@low = Hash.new()
 		@open = Hash.new()
 		@info_array = Array.new()
-		@highest
-		@lowest
+
+		@data.each do |e| 
+			@high[e.trade_date] = e.high 
+			@low[e.trade_date] = e.low 
+			@open[e.trade_date] = e.open 
+		end 
+
+		@info_array = ["High", @high, "Low", @low, "Open", @open]
+
+		line_chart (0..2).map {|e| 
+			{ 
+				name: @info_array[e*2], 
+				data: @info_array[e*2+1]
+			}
+		}, 
+		min:500, 
+		colors:["#6600FF", "#FF0066", "#66CCFF"],
+		library: 
+			{
+				backgroundColor: "#000006", 
+				legend: 
+					{
+						textStyle:{color: "#dadada"}
+					}
+			}
 	end
 end
