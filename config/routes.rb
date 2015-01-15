85397f81be1add1381396f60468752b77bd958e6
Callum Guthrie <callumguthrie@gmail.com>
2015-01-15 23:36:15 +0000

    Updated configs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   
-
-  get 'users/new'
-=======
-  	get 'sessions/new'
->>>>>>> master
-
-  	post 'sessions/create'
-
-  	get 'sessions/destroy'
-
-  	get 'sessions/login'
-
-  	get 'sessions/logout'
-
+	get 'sessions/new'
+	post 'sessions/create'
+	get 'sessions/destroy'
+	get 'sessions/login'
+	get 'sessions/logout'
 	get 'dashboard/show'
-
 	get '/login' => 'sessions#new'
-
 	get '/logout' => 'sessions#destroy'
-	
-<<<<<<< HEAD
-	root to: 'dashboard#show'
+		root to: 'sessions#new'		
+	resources :meetings
 
-  resources :meetings
-
-=======
-	root to: 'sessions#new'
->>>>>>> master
 end
