5019cba6be68ea13325997a4b9c61241df242fdb
helen <hr80@hw.ac.uk>
2015-01-17 20:34:06 +0000

    Added view client functionality, client details validation

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,10 +1,17 @@
 class ClientsController < ApplicationController
+	def index
+		@clients = Client.all
+	end
 	def new
+		@client = Client.new
 	end
 	def create
 		@client = Client.new(params.require(:client).permit(:first_name, :last_name, :dob, :address, :home_tel, :mobile_tel, :email))
-		@client.save
-		redirect_to @client
+		if @client.save
+			redirect_to @client
+		else
+			render 'new'
+		end
 	end
 	def show
 		@client = Client.find(params[:id])
