db3baa621e7ab15c327ba81e302b01ec68327ac4
helen <hr80@hw.ac.uk>
2015-01-19 01:13:35 +0000

    Added edit and delete functionality for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,18 +2,39 @@ class ClientsController < ApplicationController
 	def index
 		@clients = Client.all
 	end
+	def show
+		@client = Client.find(params[:id])
+	end
 	def new
 		@client = Client.new
 	end
+	def edit
+		@client = Client.find(params[:id])
+	end
+	def update
+		@client = Client.find(params[:id])
+		if @client.update(client_params)
+			redirect_to @client
+		else
+			render 'edit'
+		end
+	end
 	def create
-		@client = Client.new(params.require(:client).permit(:first_name, :last_name, :dob, :address, :home_tel, :mobile_tel, :email))
+		@client = Client.new(client_params)
 		if @client.save
 			redirect_to @client
 		else
 			render 'new'
 		end
 	end
-	def show
+	def destroy
 		@client = Client.find(params[:id])
+		@client.destroy
+	
+		redirect_to clients_path
+	end
+	private
+	def client_params
+	params.require(:client).permit(:first_name, :last_name, :dob, :address, :home_tel, :mobile_tel, :email)	
 	end
 end
