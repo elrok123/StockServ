0f7c4cef463b5f8e686e3012d7723d31fe5f860c
Conner Stephen McCabe <conner@koansystems.co.uk>
2015-01-22 15:42:45 +0000

    Some changes to app, and removed unnecessary Users column

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,21 +3,24 @@ class Meeting < ActiveRecord::Base
 				length: { 
 					  minimum: 2,
 					  maximum: 20,
-					  too_short: "Must be longer title"
+					  too_short: "Must be longer title",
 					  too_long: "Title too long"
 					}
 
-    validates :date_of_meeting, presence: true, 
-				  format: { with: ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$,
-				 message: "Date format incorrect. Enter in dd/mm/yyyy style"
+    validates :date_of_meeting, 
+    			presence: {message: "Date format incorrect. Enter in dd/mm/yyyy style"}
+				#format: { with: /\A((((0[13578])|([13578])|(1[02]))[\/](([1-9])|([0-2][0-9])|(3[01])))|(((0[469])|([469])|(11))[\/](([1-9])|([0-2][0-9])|(30)))|((2|02)[\/](([1-9])|([0-2][0-9]))))[\/]\d{4}$|^\d{4}\z/}
+				
 
-    validates :client_name, presence: true,
-			      length: {
-					  minimum: 2,
-                                          maximum: 20,
-                                          too_short: "Name too short"
-                                          too_long: "Name too long"
-                                       }
+    validates :client_name, 
+    			presence: true,
+				length: {
+				  minimum: 2,
+				  maximum: 20,
+				  too_short: "Title too short",
+				  too_long: "Title too long"
+				}
 
     validates :meeting_description, length: { maximum: 1000 }
 end
+#
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113183512) do
+ActiveRecord::Schema.define(version: 20150122153946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 20150113183512) do
     t.string   "email"
     t.string   "password_salt"
     t.string   "password_hash"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "Users"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.string   "firstname"
     t.string   "surname"
+    t.integer  "access_level",  default: 0
   end
 
 end
