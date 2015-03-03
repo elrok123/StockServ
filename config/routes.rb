aac630eb48ca8531ec401f4b52f7709a53425f3b
Callum Guthrie <callumguthrie@gmail.com>
2015-03-03 11:15:40 +0000

    Getting up to date after period of absence

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,23 @@
 
 Rails.application.routes.draw do
   
-	get 'sessions/new'
-	post 'sessions/create'
-	get 'sessions/destroy'
-	get 'sessions/login'
-	get 'sessions/logout'
-	get 'dashboard/show'
-	get '/login' => 'sessions#new'
-	get '/logout' => 'sessions#destroy'
-<<<<<<< HEAD
-<<<<<<< HEAD
-	root to: 'sessions#new'		
-	
-	resources :meetings
+ get 'calendar/show'
+get 'sessions/new'
+post 'sessions/create'
+get 'sessions/destroy'
+get 'sessions/login'
+get 'sessions/logout'
+get 'dashboard/show'
+get '/login' => 'sessions#new'
+get '/logout' => 'sessions#destroy'
+get 'market/show'
+get 'profile/view'
+get 'profile/edit'
+post 'dashboard/show'
+get 'clients/show'
+post 'meetings/create'
+root to: 'sessions#new'
+resources :meetings
+match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
+resources :clients
 end
-=======
-=======
-	get 'market/show'
-	get 'profile/view'
-	get 'profile/edit'
-	post 'dashboard/show'
-	get 'clients/show'
-	post 'meetings/create'
->>>>>>> master
-	
-	root to: 'sessions#new'
-
-	resources :meetings
- 	match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
-	resources :clients
-
-end
->>>>>>> master
--- test/models/memo_test.rb
+++ test/models/memo_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ClientsControllerTest < ActionController::TestCase
+class MemoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
